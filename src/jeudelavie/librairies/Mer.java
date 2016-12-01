ae9e21dfe43da74b08f58cd229aef528723e5ff6
Jesus Garnica <21509313@etu.unicaen.fr>
2016-12-01 15:49:38 +0100

    mer.java modified

--- src/jeudelavie/librairies/Mer.java
+++ src/jeudelavie/librairies/Mer.java
@@ -1,20 +1,74 @@
 package jeudelavie.librairies;
 
-public class Mer {
-	
-	private int cyclesCount;
+import java.util.*;
+import java.io.*;
 
-	public Mer(int sardinesCount, int requinsCount) {
-		
-	}
-	
-	public void setCyclesCount(int cyclesCount) {
-		
-		this.cyclesCount = cyclesCount;
-	}
 
-	public void start() {
-		
-	}
+class Mer {
+
+  private final static int SEA_SIZE = 5;
+  private final static Object EMPTY = null;
+
+  private int width;
+  private int height;
+  private int numSardines;
+  private int numRequins;
+  private Object[][] sea;
+
+  public Mer (int width, int height, int numSardines, int numRequins)
+  {
+    this.width = width;
+    this.height = height;
+    this.sardines = new Sardine[numSardines];
+    this.requins = new Requin[numRequins];
+    this.sea = new Object[width][height];
+  }
+
+  public Mer (int numSardines, int numRequins)
+  {
+    this(SEA_SIZE,SEA_SIZE,numSardines,numRequins);
+  }
+
+  public void initializeSea()
+  {
+  	for (int i = 0; i < this.width; i++)
+    {
+  		for (int j = 0; j < this.height; j++)
+      {
+  			this.sea[i][j]=EMPTY;
+  		}
+  	}
+  }
+
+  public void creerSardines ()
+  {
+    for (int i = 0; i < this.numSardines.lenght; i++)
+    {
+      this.sardines[i] = new Sardine();
+    }
+  }
+
+  public void creerRequins ()
+  {
+    for (int i = 0; i < this.numRequins.lenght; i++)
+    {
+      this.requins[i] = new Sardine();
+    }
+  }
+
+  public void displaySea()
+  {
+   for (int i = 0; i < this.width; i++)
+   {
+    for (int j = 0; j < this.height; j++)
+    {
+     if (this.sea[i][j] == EMPTY)
+      System.out.print("[X]");
+     else
+     System.out.print(this.sea[i][j] + "\t");
+    }
+    System.out.print("\n");
+  }
 }
 
+}
