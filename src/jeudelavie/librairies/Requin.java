731930d5ced784bba9fa95113064ed09dbf3c851
hivinau <hivinau.graffe@hotmail.fr>
2016-12-01 14:32:23 +0100

    Last commit

---
+++ src/jeudelavie/librairies/Requin.java
@@ -0,0 +1,14 @@
+package jeudelavie.librairies;
+
+/**
+ * <b>Requin <u>extends</u> {@link jeudelavie.librairies.Poisson}</b><br>
+ * @author <ul><li>Jesus GARNICA OLARRA.</li><li>Hivinau GRAFFE.</li></ul>
+ * @version 1.0
+ */
+public final class Requin extends Poisson {
+
+	public Requin() {
+
+	}
+
+}
---
+++ src/jeudelavie/librairies/Sardine.java
@@ -0,0 +1,14 @@
+package jeudelavie.librairies;
+
+/**
+ * <b>Sardine <u>extends</u> {@link jeudelavie.librairies.Poisson}</b><br>
+ * @author <ul><li>Jesus GARNICA OLARRA.</li><li>Hivinau GRAFFE.</li></ul>
+ * @version 1.0
+ */
+public final class Sardine extends Poisson {
+
+	public Sardine() {
+		
+	}
+
+}
